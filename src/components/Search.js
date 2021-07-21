592195fb5aa224007368991e40c16286cac4111f
Mia Casas <miacasas@mias-mbp.attlocal.net>
2021-07-20 22:38:14 -0500

    modals and alerts

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,21 @@
-import React, {useState} from 'react'
+import React, {useState, render} from 'react'
 import {Media, Button} from 'reactstrap'
 import {FaPrescription} from 'react-icons/fa'
 
 const Search = () => {
-    const [search, setSearch] = useState({
-        drug: ''
-    })
-    const onSubmitForm = (e) => {
-        e.preventDefault();
-        const {drug} = e.target
-        setSearch({
-            drug: drug.value
-        })
-    }
-    
+    // const [search, setSearch] = useState({
+    //     drug: ''
+    // })
+    // const onSubmitForm = (e) => {
+    //     e.preventDefault();
+    //     const {drug} = e.target
+    //     setSearch({
+    //         drug: drug.value
+    //     })
+    // }
+
+
+
     return(
         <div>
 
@@ -49,11 +51,11 @@ const Search = () => {
     <div class="col" >
              <ul class="results-lists">
              <h3 class="results">Cost</h3>
-                <li id="move-right">$9.45  <Button id="select-rx"><FaPrescription /></Button></li>
-                <li id="move-right">$14.99 <Button id="select-rx"><FaPrescription /></Button></li>
-                <li id="move-right">$20.40 <Button id="select-rx"><FaPrescription /></Button></li>
-                <li id="move-right">$21.15 <Button id="select-rx"><FaPrescription /></Button></li>
-                <li id="move-right">$21.15 <Button id="select-rx"><FaPrescription /></Button></li>
+                <li id="move-right">$9.45  <Button id="select-rx" onClick={()=>alert('Send your prescription to this pharmacy?') }><FaPrescription /></Button></li> 
+                <li id="move-right">$14.99 <Button id="select-rx" onClick={()=>alert('Send your prescription to this pharmacy?') }><FaPrescription /></Button></li>
+                <li id="move-right">$20.40 <Button id="select-rx" onClick={()=>alert('Send your prescription to this pharmacy?') }><FaPrescription /></Button></li>
+                <li id="move-right">$21.15 <Button id="select-rx" onClick={()=>alert('Send your prescription to this pharmacy?') }><FaPrescription /></Button></li>
+                <li id="move-right">$21.15 <Button id="select-rx" onClick={()=>alert('Send your prescription to this pharmacy?') }><FaPrescription /></Button></li>
             </ul>
     </div>
   </div>
--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,20 @@
-import React from 'react'
-import {Media} from 'reactstrap'
+import React, {useState} from 'react'
+import {Media, Modal, ModalFooter, ModalBody, ModalHeader, Input, Button} from 'reactstrap'
 
 const User = () => {
+
+
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+  
+  const [modal2, setModal2] = useState(false);
+  const toggle2 = () => setModal2(!modal2)
+
+  const [modal3, setModal3] = useState(false);
+  const toggle3 = () => setModal3(!modal3)
+
+  const [modal4, setModal4] = useState(false);
+  const toggle4 = () => setModal4(!modal4)
     return(
         <div>
         <div >
@@ -22,10 +35,78 @@ const User = () => {
         </div>
 
         <div class="d-grid gap-2 col-6 mx-auto" id="user-buttons">
-        <button class="btn btn-primary" type="button">Profile</button>
-        <button class="btn btn-primary" type="button">Order History</button>
-        <button class="btn btn-primary" type="button">My Prescriptions</button>
-        <button class="btn btn-primary" type="button">Payment Information</button>
+
+        <button class="btn btn-primary" type="button" onClick={toggle}>Profile</button>
+            <Modal isOpen={modal} toggle={toggle}>
+            <ModalHeader>My Profile</ModalHeader>
+            <ModalBody>
+                <ul>
+                    <li>Phone Number: (317)-123-4567</li>
+                    <li>Email Address: john.smith@email.com</li>
+                    <li>Default Pharmacy: Walgreens</li>
+                    <br></br>
+                    <Button>Edit</Button>
+                </ul>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="success" type="submit" onClick={toggle}>Update</Button>
+                <Button color="danger" type="submit" onClick={toggle}>Exit</Button>
+            </ModalFooter>
+
+        </Modal>
+        <button class="btn btn-primary" type="button" onClick={toggle2}>Order History</button>
+        <Modal isOpen={modal2} toggle={toggle2}>
+            <ModalHeader>Order History</ModalHeader>
+            <ModalBody>
+                <table>
+                    <tr>
+                        <th>Date</th>
+                        <th>Medicine</th>
+                        <th>Status</th>
+                    </tr>
+                </table>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="danger" type="submit" onClick={toggle2}>Exit</Button>
+            </ModalFooter>
+
+        </Modal>
+        <button class="btn btn-primary" type="button" onClick={toggle3}>My Prescriptions</button>
+        <Modal isOpen={modal3} toggle={toggle3}>
+            <ModalHeader>My Prescriptions</ModalHeader>
+            <ModalBody>
+                <table>
+                    <tr>
+                        <th>Medicine</th>
+                        <th>Refills Available</th>
+                        <th>Expiration</th>
+                    </tr>
+                </table>
+                <Button>Get Refill</Button>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="danger" type="submit" onClick={toggle3}>Exit</Button>
+            </ModalFooter>
+
+        </Modal>
+        <button class="btn btn-primary" type="button" onClick={toggle4}>Payment Information</button>
+        <Modal isOpen={modal4} toggle={toggle4}>
+            <ModalHeader>Payment Information</ModalHeader>
+            <ModalBody>
+                <ul>
+                    <li>Card Number: xxxx-xxxx-xxxx-1111</li>
+                    <li>CVV: 222</li>
+                    <li>Expiration Date: 03/04</li>
+                    <br></br>
+                    <Button>Edit</Button>
+                </ul>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="success" type="submit" onClick={toggle4}>Update</Button>
+                <Button color="danger" type="submit" onClick={toggle4}>Exit</Button>
+            </ModalFooter>
+
+        </Modal>
         </div>
         </div>
     )
